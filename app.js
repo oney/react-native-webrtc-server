150771a4c20f5612e4ae8f4f0c2f62dd8d8b2fa9
SHANG-YU, SU <sam7822567@gmail.com>
2016-07-16 22:51:17 +0800

    change to es6 syntax with airbnb aslant coding style

--- app.js
+++ app.js
@@ -1,72 +1,71 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var open = require('open');
-var options = {
+'use strict';
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const open = require('open');
+const options = {
   key: fs.readFileSync('./fake-keys/privatekey.pem'),
-  cert: fs.readFileSync('./fake-keys/certificate.pem')
+  cert: fs.readFileSync('./fake-keys/certificate.pem'),
 };
-var serverPort = (process.env.PORT  || 4443);
-var https = require('https');
-var http = require('http');
-var server;
+const serverPort = (process.env.PORT || 4443);
+const https = require('https');
+const http = require('http');
+let server;
 if (process.env.LOCAL) {
   server = https.createServer(options, app);
 } else {
   server = http.createServer(app);
 }
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
-var roomList = {};
-
-app.get('/', function(req, res){
-  console.log('get /');
-  res.sendFile(__dirname + '/index.html');
-});
-server.listen(serverPort, function(){
-  console.log('server up and running at %s port', serverPort);
-  if (process.env.LOCAL) {
-    open('https://localhost:' + serverPort)
-  }
-});
-
-function socketIdsInRoom(name) {
-  var socketIds = io.nsps['/'].adapter.rooms[name];
+const socketIdsInRoom = (name) => {
+  const socketIds = io.nsps['/'].adapter.rooms[name];
   if (socketIds) {
-    var collection = [];
-    for (var key in socketIds) {
+    const collection = [];
+    for (const key in socketIds) {
       collection.push(key);
     }
     return collection;
-  } else {
-    return [];
   }
-}
+  return [];
+};
 
-io.on('connection', function(socket){
+app.get('/', (req, res) => {
+  console.log('get /');
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(serverPort, () => {
+  console.log('server up and running at %s port', serverPort);
+  if (process.env.LOCAL) {
+    open(`https://localhost: ${serverPort}`);
+  }
+});
+
+io.on('connection', (socket) => {
   console.log('connection');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     console.log('disconnect');
     if (socket.room) {
-      var room = socket.room;
+      const room = socket.room;
       io.to(room).emit('leave', socket.id);
       socket.leave(room);
     }
   });
 
-  socket.on('join', function(name, callback){
+  socket.on('join', (name, callback) => {
     console.log('join', name);
-    var socketIds = socketIdsInRoom(name);
+    const socketIds = socketIdsInRoom(name);
     callback(socketIds);
     socket.join(name);
     socket.room = name;
   });
 
-
-  socket.on('exchange', function(data){
+  socket.on('exchange', (data) => {
     console.log('exchange', data);
     data.from = socket.id;
-    var to = io.sockets.connected[data.to];
+    const to = io.sockets.connected[data.to];
     to.emit('exchange', data);
   });
 });
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(`${__dirname}/index.html`);
